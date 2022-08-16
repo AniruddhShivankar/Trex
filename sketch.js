1b0f69bb590e07fbd5a44757926a9bc763a99ac6
AniruddhShivankar <111354117+AniruddhShivankar@users.noreply.github.com>
2022-08-16 18:58:10 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(600,200);
+
+
+ 
+function draw(){
+ 
+}
+
+var s = "";
+for(var i = 1; i < 11; i += 1) {
+  s += i + " ";
+}
+console.log(s);
